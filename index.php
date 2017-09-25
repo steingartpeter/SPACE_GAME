54fb938de8b5d92ef52310794f00134b6acf5a88
steingartpeter <steingart.p2@gmail.com>
2017-09-25 19:43:08 +0200

    2017-09-25 002 AX07057 HOME

--- index.php
+++ index.php
@@ -10,8 +10,22 @@
 
 
 ?>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Page 1</a></li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
 
-<div class="container-fluid">
+
+<div class="container">
   <h1>My First Bootstrap Page</h1>
   <p>This is some text.</p>
 </div>
