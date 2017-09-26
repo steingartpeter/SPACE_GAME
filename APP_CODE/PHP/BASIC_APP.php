03db666b8137c79b010dc3441381b3dc4daf5b11
steingartpeter <steingart.p2@gmail.com>
2017-09-26 10:50:55 +0200

    2017-09-26 001 AX07057 HAL

--- APP_CODE/PHP/BASIC_APP.php
+++ APP_CODE/PHP/BASIC_APP.php
@@ -45,18 +45,74 @@ class APP0924{
         $html = "";
         
         $html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/jquery/jquery-3.2.1.min.js"></script>';
-        $html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/popper/popper.min.js"></script>';
-        $html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/bootstrap/bootstrap.min.js"></script>';
+        //$html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/popper/popper.min.js"></script>';
+        //$html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/bootstrap/bootstrap.min.js"></script>';
+        $html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/bootstrap337/js/bootstrap.min.js"></script>';
         $html .= '<script src="/SPACE_GAME/APP_CODE/JSCRIPT/app.js"></script>';
         
-        $html .= '<link rel="stylesheet" href="/SPACE_GAME/STYLE/bootstrap/bootstrap.min.css" />';
+        $html .= '<link rel="stylesheet" href="/SPACE_GAME/APP_CODE/JSCRIPT/LIBS/bootstrap337/css/bootstrap.min.css" />';
         $html .= '<link rel="stylesheet" href="/SPACE_GAME/STYLE/bootstrap/bootstrap-grid.min.css" />';
+        $html .= '<link rel="stylesheet" href="/SPACE_GAME/STYLE/bootstrap/bootstrap-reboot.min.css" />';
         $html .= '<link rel="stylesheet" href="/SPACE_GAME/STYLE/basic_style.css" />';
         
         
         return $html;
     }
     
+    
+    public function createSTDNAV(){
+    //<SF>
+    // 2017. szept. 26.<br>
+    // A standard boostrapos NAV elem legenrálása, jelenleg javarészt dummy elemekkel.<br>
+    // PARAMÉTEREK:
+    //×-
+    // @-- @param ... = ... -@
+    //-×
+    //MÓDOSTÁSOK:
+    //×-
+    // @-- ... -@
+    //-×
+    //</SF>
+    	$html = "";
+    	$html .= '<!-- STANDARD NAV generálása PHP ból -->';
+    	
+    	$html .= '<nav class="navbar navbar-inverse navbar-fixed-top">';
+    	$html .= '<div class="container-fluid">';
+    	$html .= '<div class="navbar-header">';
+    	$html .= '<a class="navbar-brand" href="#">Space Project 001</a>';
+    	$html .= '</div>';
+    	$html .= '<div>';
+    	$html .= '<ul class="nav navbar-nav">';
+    	$html .= '<li class="active"><a href="#">START</a></li>';
+    	$html .= '<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Lap 1</a>';
+    	$html .= '<ul class="dropdown-menu">';
+    	$html .= '<li><a href="#">1.1 Menuelem</a></li>';
+    	$html .= '<li><a href="#">1.2 Menuelem</a></li>';
+    	$html .= '<li><a href="#">1.3 Menuelem</a></li>';
+    	$html .= '</ul>';
+    	$html .= '</li>';
+    	$html .= '<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Lap 2</a>';
+    	$html .= '<ul class="dropdown-menu">';
+    	$html .= '<li><a href="#">2.1 Menuelem</a></li>';
+    	$html .= '<li><a href="#">2.2 Menuelem</a></li>';
+    	$html .= '<li><a href="#">2.3 Menuelem</a></li>';
+    	$html .= '</ul>';
+    	$html .= '</li>';
+    	$html .= '<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown">Lap 3</a>';
+    	$html .= '<ul class="dropdown-menu">';
+    	$html .= '<li><a href="#">3.1 Menuelem</a></li>';
+    	$html .= '<li><a href="#">3.2 Menuelem</a></li>';
+    	$html .= '<li><a href="#">2.3 Menuelem</a></li>';
+    	$html .= '</ul>';
+    	$html .= '</li>';
+    	$html .= '</ul>';
+    	$html .= '</div>';
+    	$html .= '</div>';
+    	$html .= '</nav>';
+    	
+    	return $html;
+    	
+    }
     
     public function createSiteLowerPart(){
     //<SF>
@@ -67,7 +123,7 @@ class APP0924{
         
         $html .= "</body></html>";
         
-        return $tml;
+        return $html;
         
     }
     
--- index.php
+++ index.php
@@ -8,27 +8,23 @@
     
     echo $pgGnrtr->createHeaderLinks();
 
-
+	echo $pgGnrtr->createSTDNAV();
 ?>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
-    </ul>
-  </div>
-</nav>
 
+
+
+<div class="container">
+	<div class="jumbotron" id="jumb-start">
+		UNIVERSE-GENERATOR
+	</div>
+</div>
 
 <div class="container">
   <h1>My First Bootstrap Page</h1>
   <p>This is some text.</p>
 </div>
+
+
 
 <?php 
 
